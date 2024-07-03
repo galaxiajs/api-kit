86eb95242aad57344b9c383ed1a9e846659785a3
Ernest Badu <ernestjbadu@outlook.com>
2024-07-03 19:51:10 +0100

    test: use vitest workspaces

--- packages/cloudflare/vitest.config.ts
+++ packages/cloudflare/vitest.config.ts
@@ -1,21 +1,12 @@
-import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 import paths from "vite-tsconfig-paths";
 
 const COMPATIBILITY_DATE = "2024-04-05";
 const COMPATIBILITY_FLAGS = ["nodejs_compat"];
 
-export default defineWorkersConfig({
+export default defineWorkersProject({
 	plugins: [paths()],
-	esbuild: {
-		// https://github.com/vitejs/vite/issues/15464#issuecomment-1872485703
-		target: "es2020",
-	},
 	test: {
-		coverage: {
-			all: true,
-			reportOnFailure: true,
-			provider: "istanbul",
-		},
 		poolOptions: {
 			workers: {
 				singleWorker: true,
